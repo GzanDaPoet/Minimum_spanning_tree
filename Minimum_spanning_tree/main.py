00939e9b5b972359ceb68e54870a94a5291484c3
GzanDaPoet <nguyentoan.13.9.2002@gmail.com>
2023-01-04 20:59:03 +0700

    Toan1

--- Minimum_spanning_tree/main.py
+++ Minimum_spanning_tree/main.py
@@ -6,6 +6,7 @@ from Prim import prim
 from Kruskal import kruskal_algo
 from DrawGraph import draw_graph
 
+
 class MainWindow:
     def __init__(self):
         self.main_win = QtWidgets.QMainWindow()
@@ -13,14 +14,26 @@ class MainWindow:
         self.uic.setupUi(self.main_win)
         self.uic.btnKruscal.clicked.connect(self.dokruscal)
         self.uic.btnPrim.clicked.connect(self.doprim)
-        # self.uic.btnInfo.clicked.connect(self.)
+        self.uic.btnInfo.clicked.connect(self.showInfo)
         self.uic.btnNext.clicked.connect(self.donext)
         self.uic.btnPrev.clicked.connect(self.doPrev)
         self.count = 0
         self.length = 0
         self.arrayDraw = []
 
-    # def showInfo(self):
+    def showInfo(self, m, n):
+        self.uic.tblMaTran.setRowCount(m)
+        self.uic.tblMaTran.setColumnCount(m)
+        lstMaTran = n.split("\n")
+        for i in range(m):
+            lstMaTran[i] = lstMaTran[i].split(", ")
+        for x in range(m):
+            self.uic.tblMaTran.setRowHeight(x,1)
+            self.uic.tblMaTran.setColumnWidth(x,1)
+            for y in range(m):
+                self.uic.tblMaTran.setItem(x, y, QtWidgets.QTableWidgetItem(lstMaTran[x][y]))
+        print("end")
+
 
     def donext(self):
         if self.length >= self.count + 1:
@@ -28,12 +41,15 @@ class MainWindow:
             draw_graph(self.arrayDraw[:self.count:])
             qpixMap = QPixmap("graph.png")
             self.uic.image_label.setPixmap(qpixMap)
+
+
     def doPrev(self):
         if self.count - 1 >= 0:
             self.count = self.count - 1
             draw_graph(self.arrayDraw[:self.count:])
             qpixMap = QPixmap("graph.png")
             self.uic.image_label.setPixmap(qpixMap)
+
     def dokruscal(self):
         a = int(self.uic.txtSoDinh.text())
         b = self.uic.txtMaTran.toPlainText()
@@ -45,6 +61,9 @@ class MainWindow:
         self.length = len(kruskal)
         self.arrayDraw = kruskal
         self.count = 1
+        self.showInfo(a, b)
+        qpixMap = QPixmap()
+        self.uic.image_label.setPixmap(qpixMap)
 
     def doprim(self):
         rs = ""
@@ -57,10 +76,10 @@ class MainWindow:
         self.length = len(arrPrim)
         self.count = 1
         self.arrayDraw = arrPrim
+        self.showInfo(a, b)
+        qpixMap = QPixmap()
+        self.uic.image_label.setPixmap(qpixMap)
 
-    # def showinfo(self):
-    #     self.uic.pushButton.setEnabled(False)
-    #     a = self.uic.lineEdit.text()
 
     def show(self):
         self.main_win.show()
@@ -72,11 +91,3 @@ if __name__ == "__main__":
     main_win.show()
     sys.exit(app.exec())
 
-# if __name__ == "__main__":
-#     app = QtWidgets.QApplication(sys.argv)
-#     MainWindow = QtWidgets.QMainWindow()
-#     ui = Ui_MainWindow()
-#     ui.setupUi(MainWindow)
-#     MainWindow.show()
-#     ui.pushButton_2.clicked.connect(ui.lineEdit.setText(self="S123"))
-#     sys.exit(app.exec_())
